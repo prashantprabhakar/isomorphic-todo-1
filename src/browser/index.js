38cc71b51f2ad9336bb86074db6e7a49f022b6e3
prashant.prabhakar <prashantprabhakar123@gmail.com>
2019-11-13 14:23:29 +0530

    removed extra code

--- src/browser/index.js
+++ src/browser/index.js
@@ -1,13 +1,8 @@
 import React from "react";
 import { render } from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-//import App from "../shared/App";
 import Todo from "../shared/todo/Todo"
 
 render(
-  // <BrowserRouter>
-  //   <App />
-  // </BrowserRouter>,
   <Todo />,
   document.getElementById("root")
 );
--- src/server/index.js
+++ src/server/index.js
@@ -2,11 +2,6 @@ import express from "express";
 import cors from "cors";
 import React from "react";
 import { renderToString } from "react-dom/server";
-// import { StaticRouter, matchPath } from "react-router-dom";
-// import serialize from "serialize-javascript";
-// import routes from "../shared/routes";
-// import App from "../shared/App";
-// import sourceMapSupport from "source-map-support";
 import Todo from "../shared/todo/Todo"
 import "isomorphic-fetch";
 import bodyParser from "body-parser"
@@ -86,10 +81,6 @@ let initData = [
 ]
 
 
-// if (process.env.NODE_ENV === "development") {
-//   sourceMapSupport.install();
-// }
-
 const app = express();
 
 app.use(cors());
@@ -126,40 +117,6 @@ app.get("*", async(req, res) => {
     </html>
   `)
 })
-
-// app.get("*", (req, res, next) => {
-//   const activeRoute = routes.find(route => matchPath(req.url, route));
-
-//   const requestInitialData =
-//     activeRoute.component.requestInitialData && activeRoute.component.requestInitialData();
-
-//   Promise.resolve(requestInitialData)
-//     .then(initialData => {
-//       const context = { initialData };
-//       const markup = renderToString(
-//         <StaticRouter location={req.url} context={context}>
-//           <App />
-//         </StaticRouter>
-//       );
-
-//       res.send(`
-//       <!DOCTYPE html>
-//       <html>
-//         <head>
-//           <title>W Combinator</title>
-//           <link rel="stylesheet" href="/css/main.css">
-//           <script src="/bundle.js" defer></script>
-//           <script>window.__initialData__ = ${serialize(initialData)}</script>
-//         </head>
-
-//         <body>
-//           <div id="root">${markup}</div>
-//         </body>
-//       </html>
-//       `);
-//     })
-//     .catch(next);
-// });
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server is listening");
